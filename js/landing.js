386c2c4ca16dd7c6d9d69d13890bcabd7b883c50
egaeuni <gaeun3619@gmail.com>
2025-05-08 12:16:04 +0900

    ✨ 250508 Landing page

---
+++ js/landing.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  var mySwiper = new Swiper(".swiper-container", {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+  });
+});
